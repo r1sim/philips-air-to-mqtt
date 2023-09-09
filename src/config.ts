3b09714e9ddc9767f21dff6663db4d89b775495f
Simon Risse <9327096+r1sim@users.noreply.github.com>
2023-09-10 00:06:42 +0200

    fix: add config validation

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
-export default {
+import Joi from 'joi';
+
+const config = {
   mqtt: {
     connection: {
       host: (process.env.MQTT_HOST as string) ?? 'localhost',
@@ -33,3 +35,37 @@ export default {
     },
   },
 };
+
+const validationSchema = Joi.object({
+  mqtt: Joi.object({
+    connection: Joi.object({
+      host: Joi.alternatives(
+        Joi.string().hostname(),
+        Joi.string().ip()
+      ).required(),
+      port: Joi.number().port().required(),
+      username: Joi.string().allow('').optional(),
+      password: Joi.string().allow('').optional(),
+    }).required(),
+    homeAssistantAutoDiscovery: Joi.object({
+      enabled: Joi.boolean().required(),
+      deleteOnShutdown: Joi.boolean().required(),
+    }).required(),
+    topicPrefix: Joi.string().required(),
+  }).required(),
+  airPurifier: Joi.object({
+    deviceName: Joi.string().required(),
+    refreshInterval: Joi.number().required(),
+    connection: Joi.object({
+      host: Joi.string().required(),
+      protocol: Joi.string().valid('http', 'coap').required(),
+    }).required(),
+  }).required(),
+});
+
+const { error, value } = validationSchema.validate(config);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+export default value;
